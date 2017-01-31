d480ad3b6b9bf27446148fded5ec0d479d53ae69
odknt <4185305+odknt@users.noreply.github.com>
2017-02-01 00:27:33 +0900

    [i3] add a script for check GPU temperature

---
+++ scripts/gputemp.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VGAOUT="$(lspci | grep -wi vga)"
+RADEON="$(grep -wi radeon <<< "$VGAOUT")"
+NVIDIA="$(grep -wi nvidia <<< "$VGAOUT")"
+
+if [[ $NVIDIA ]]; then
+  echo "$(nvidia-settings -q '[gpu:0]/GPUCoreTemp' | \
+    grep -Po '(?<=\):\s)\d+(?=.)')℃"
+fi
