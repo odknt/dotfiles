b591d55fb93d274d21ef4f63ab45474fac8437ee
odknt <4185305+odknt@users.noreply.github.com>
2017-02-01 10:49:34 +0900

    [i3] doesn't output title if no window

--- scripts/title.sh
+++ scripts/title.sh
@@ -3,6 +3,10 @@
 while :; do
   WID=$(xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print $NF}')
   NAME=$(xprop -id $WID | awk '/_NET_WM_NAME/{$1=$2="";print}' | cut -d'"' -f2)
-  echo " $NAME "
+  if [[ -n $NAME ]]; then
+    echo " $NAME "
+  else
+    echo
+  fi
   perl -e 'use Time::HiRes "sleep"; sleep(0.2);'
 done
