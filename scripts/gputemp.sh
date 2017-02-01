3a97da56d17f50725636c1d94bbfb7d3a840d0c4
odknt <4185305+odknt@users.noreply.github.com>
2017-02-01 10:26:26 +0900

    [i3] doesn't output GPU temperature if scan failed

--- scripts/gputemp.sh
+++ scripts/gputemp.sh
@@ -5,6 +5,8 @@ RADEON="$(grep -wi radeon <<< "$VGAOUT")"
 NVIDIA="$(grep -wi nvidia <<< "$VGAOUT")"
 
 if [[ $NVIDIA ]]; then
-  echo "$(nvidia-settings -q '[gpu:0]/GPUCoreTemp' | \
+  echo "  $(nvidia-settings -q '[gpu:0]/GPUCoreTemp' | \
     grep -Po '(?<=\):\s)\d+(?=.)')℃"
+else
+  exit 1
 fi
