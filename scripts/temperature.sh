bcb8aeff3737cbe9bf26726760b5bbe5323e864e
odknt <4185305+odknt@users.noreply.github.com>
2017-02-02 19:25:07 +0900

    [scripts] fix bug

--- scripts/temperature.sh
+++ scripts/temperature.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 OUTPUT="$(sensors -Au)"
 
-IFS=$'\n\t'
-DATA="$(echo "${OUTPUT}" | egrep -A4 "${CORE}")"
-CURRENT="$(echo "${DATA}" | grep -Po "(?<=temp1_input: )([0-9]+)")"
-HIGH="$(echo "${DATA}" | grep -Po "(?<=temp1_max: )([0-9]+)")"
+DATA="$(echo "${OUTPUT}")"
+CURRENT="$(echo "${DATA}" | grep -Po "(?<=temp1_input: )([0-9]+)" | head -n1)"
+HIGH="$(echo "${DATA}" | grep -Po "(?<=temp1_max: )([0-9]+)" | head -n1)"
 #CRIT="$(echo "${DATA}" | grep -Po "(?<=temp1_crit: )([0-9]+)")"
 
-echo -n "${CURRENT}℃ "
-[[ ${t} -gt ${HIGH} ]] && exit 33
+echo "${CURRENT}℃ "
+[[ ${CURRENT} > ${HIGH} ]] && exit 33
 
 exit 0
