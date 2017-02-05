1c55d717c97801059c5695eb03c7319130737800
odknt <4185305+odknt@users.noreply.github.com>
2017-02-06 02:41:02 +0900

    [general] update init.sh

--- init.sh
+++ init.sh
@@ -26,13 +26,37 @@ EOS
   yaourt -S --needed --noconfirm - < .native.plist
 
   while read -r p; do
-    yaourt -S --needed "$p"
+    yaourt -S --needed "${p}"
   done < .aur.plist
+
+  if [[ -z ${GOPATH:-} ]]; then
+    GOPATH="${HOME}/go"
+  fi
+
+  go get github.com/odknt/go-skkserv-google
+  init::gen_unit
+}
+
+init::gen_unit() {
+  cat <<EOF> .config/systemd/user/skkserv-google.service
+[Unit]
+Description=SKK server by using Google IME API
+
+[Service]
+ExecStart=${GOPATH}/bin/go-skkserv-google -p 55100 -n 200 /usr/share/skk/SKK-JISYO.L
+
+[Install]
+WantedBy=default.target
+EOF
+
+  systemctl --user daemon-reload
+  systemctl --user enable skkserv-google.service
 }
 
 case "$1" in
   install) init::install;;
+  gen_unit) init::gen_unit;;
   *)  init::usage;;
 esac
 
-exit $?
+exit ${?}
