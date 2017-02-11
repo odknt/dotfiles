eb65e18c3bcbde725aee436a86b87e7bd4e92a49
odknt <4185305+odknt@users.noreply.github.com>
2017-02-12 01:30:39 +0900

    [i3] fix bugs

--- scripts/gputemp.sh
+++ scripts/gputemp.sh
@@ -5,6 +5,8 @@ RADEON="$(grep -wi radeon <<< "$VGAOUT")"
 NVIDIA="$(grep -wi nvidia <<< "$VGAOUT")"
 
 if [[ -n $NVIDIA ]]; then
-  echo "   $(nvidia-settings -q '[gpu:0]/GPUCoreTemp' | \
-    grep -Po '(?<=\):\s)\d+(?=.)')℃  "
+  if type nvidia-settings 2>&1 > /dev/null; then
+    echo "   $(nvidia-settings -q '[gpu:0]/GPUCoreTemp' | \
+      grep -Po '(?<=\):\s)\d+(?=.)')℃  "
+  fi
 fi
