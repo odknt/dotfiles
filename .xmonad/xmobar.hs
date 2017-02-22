1202465f59616c739f24b8fa1e7a0cbd48003e29
odknt <4185305+odknt@users.noreply.github.com>
2017-02-23 04:10:43 +0900

    [xmonad] add xmobar config

---
+++ .xmonad/xmobar.hs
@@ -0,0 +1,110 @@
+-- vim: sw=2 ts=2 sts=2 et
+Config {
+    font = "xft:Dejavu Sans:size=9,sans-serif:size=9,FontAwesome:size=9"
+  , bgColor =      "black"
+  , fgColor =      "#646464"
+  , position =     Top
+  , borderColor =  "#646464"
+
+  -- layout
+  , sepChar =  "%"   -- delineator between plugin names and straight text
+  , alignSep = "}{"  -- separator between left-right alignment
+  , template = " %battery%   %multicpu%   %disku%   %coretemp%   %memory%   %dynnetwork% }{ %RJTT% | %date% || %kbd% "
+
+  -- general behavior
+  , lowerOnStart =     True    -- send to bottom of window stack on start
+  , hideOnStart =      False   -- start with window unmapped (hidden)
+  , allDesktops =      True    -- show on all desktops
+  , overrideRedirect = True    -- set the Override Redirect flag (Xlib)
+  , pickBroadest =     False   -- choose widest display (multi-monitor)
+  , persistent =       True    -- enable/disable hiding (True = disabled)
+
+  -- plugins
+  --   Numbers can be automatically colored according to their value. xmobar
+  --   decides color based on a three-tier/two-cutoff system, controlled by
+  --   command options:
+  --     --Low sets the low cutoff
+  --     --High sets the high cutoff
+  --
+  --     --low sets the color below --Low cutoff
+  --     --normal sets the color between --Low and --High cutoffs
+  --     --High sets the color above --High cutoff
+  --
+  --   The --template option controls how the plugin is displayed. Text
+  --   color can be set by enclosing in <fc></fc> tags. For more details
+  --   see http://projects.haskell.org/xmobar/#system-monitor-plugins.
+  , commands = 
+      -- weather monitor
+      [ Run Weather "RJTT" [ "--template", "<skyCondition> | <fc=#4682B4><tempC></fc>°C | <fc=#4682B4><rh></fc>% | <fc=#4682B4><pressure></fc>hPa"
+                           ] 36000
+
+      -- network activity monitor (dynamic interface resolution)
+      , Run DynNetwork     [ "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
+                           , "--Low"      , "1000"       -- units: kB/s
+                           , "--High"     , "5000"       -- units: kB/s
+                           , "--low"      , "darkgreen"
+                           , "--normal"   , "darkorange"
+                           , "--high"     , "darkred"
+                           ] 10
+
+      -- cpu activity monitor
+      , Run MultiCpu       [ "--template" , " <total0>% <total1>%"
+                           , "--Low"      , "50"         -- units: %
+                           , "--High"     , "85"         -- units: %
+                           , "--low"      , "darkgreen"
+                           , "--normal"   , "darkorange"
+                           , "--high"     , "darkred"
+                           ] 10
+
+      , Run DiskU          [("/", " <used>"),  ("/mnt/iomega", " <used>")]
+                           [ "-L", "20", "-H", "50", "-m", "1", "-p", "3"
+                           , "--normal", "grey50", "--high", "#a0522d"
+                           , "--low", "#2f4f4f"
+                           ] 20
+
+      -- cpu core temperature monitor
+      , Run CoreTemp       [ "--template" , " <core0>°C <core1>°C"
+                           , "--Low"      , "70"        -- units: °C
+                           , "--High"     , "80"        -- units: °C
+                           , "--low"      , "darkgreen"
+                           , "--normal"   , "darkorange"
+                           , "--high"     , "darkred"
+                           ] 50
+
+      -- memory usage monitor
+      , Run Memory         [ "--template" ," <usedratio>%"
+                           , "--Low"      , "20"        -- units: %
+                           , "--High"     , "90"        -- units: %
+                           , "--low"      , "darkgreen"
+                           , "--normal"   , "darkorange"
+                           , "--high"     , "darkred"
+                           ] 10
+
+      -- battery monitor
+      , Run Battery        [ "--template" , " <acstatus>"
+                           , "--Low"      , "10"        -- units: %
+                           , "--High"     , "80"        -- units: %
+                           , "--low"      , "darkred"
+                           , "--normal"   , "darkorange"
+                           , "--high"     , "darkgreen"
+
+                           , "--" -- battery specific options
+                                     -- discharging status
+                                     , "-o"	, "<left>% (<timeleft>)"
+                                     -- AC "on" status
+                                     , "-O"	, "<fc=#dAA520>Charging</fc>"
+                                     -- charged status
+                                     , "-i"	, "<fc=#006000>Charged</fc>"
+                           ] 50
+
+      -- time and date indicator 
+      --   (%F = y-m-d date, %a = day of week, %T = h:m:s time)
+      , Run Date           "<fc=#ABABAB>%F (%a) %T</fc>" "date" 10
+
+      -- keyboard layout indicator
+      , Run Kbd            [ ("us(dvorak)" , "<fc=#00008B>DV</fc>")
+                           , ("us"         , "<fc=#8B0000>US</fc>")
+                           ]
+      ]
+    }
+  }
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,24 +1,27 @@
 -- vim: sw=2 ts=2 sts=2 et
+import System.Exit
 import XMonad
 import XMonad.Operations
-import XMonad.Hooks.ManageDocks
-import XMonad.Layout.Gaps
-import XMonad.Layout.Spacing
 
 import XMonad.Actions.Navigation2D
-import XMonad.Actions.CopyWindow
 
 import XMonad.Config.Desktop
-import XMonad.Config.Gnome
-import XMonad.Config.Xfce
+
 import XMonad.Hooks.EwmhDesktops
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.DynamicLog
+
+import XMonad.Layout.Gaps
+import XMonad.Layout.Spacing
+import XMonad.Layout.BinarySpacePartition
+import XMonad.Layout.IndependentScreens
+
+import XMonad.Util.Run
 
 import System.IO
 import qualified Data.Map        as M
 import qualified XMonad.StackSet as W
-
-import XMonad.Layout.BinarySpacePartition
-import XMonad.Layout.IndependentScreens
 
 myLayouts = emptyBSP ||| Tall 1 (3/100) (1/2) ||| Full
 myWS = withScreens 2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -26,9 +29,11 @@ myWS = withScreens 2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 myKeys :: XConfig t -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { modMask = mask }) = M.fromList $
-  [ (( mask                 , xK_comma  ), sendMessage (IncMasterN ( 1)))
-  , (( mask                 , xK_period ), sendMessage (IncMasterN (-1)))
-  , (( mask                 , xK_d      ), spawn "dmenu_run -fn 'monospace:size=10' -h 25 -w 300 -y 25 -o 0.9 -l 10") ]
+  [ (( mask                 , xK_Return    ), spawn "lilyterm")
+  , (( mask                 , xK_comma     ), sendMessage (IncMasterN ( 1)))
+  , (( mask                 , xK_period    ), sendMessage (IncMasterN (-1)))
+  , (( mask                 , xK_b         ), sendMessage $ ToggleStruts)
+  , (( mask                 , xK_d         ), spawn "dmenu_run -fn 'monospace:size=10' -h 25 -w 300 -y 25 -o 0.9 -l 10") ]
   ++ -- Navigation2D
   [ (( mask                 , xK_h         ), windowGo L True)
   , (( mask                 , xK_j         ), windowGo D True)
@@ -48,27 +53,36 @@ myKeys conf@(XConfig { modMask = mask }) = M.fromList $
   , (( mask                 , xK_n         ), sendMessage FocusParent)
   , (( mask .|. controlMask , xK_n         ), sendMessage SelectNode)
   , (( mask .|. shiftMask   , xK_n         ), sendMessage MoveNode) ]
-  ++
+  ++ -- Switch workspace
   [ (( m .|. mask           , k         ), windows $ onCurrentScreen f i)
     | (i, k) <- zip (workspaces' conf) [ xK_1 .. xK_9 ]
     , (f, m) <- [ (W.view, 0), (W.shift, shiftMask) ]]
 
-myManage = composeAll
-  [ className =? "Ninix_main.rb" --> doIgnore ]
+role = stringProperty "WM_WINDOW_ROLE"
 
-myConfig = ewmh $ gnomeConfig
-  { terminal = "lilyterm"
-  , focusFollowsMouse = False
-  , normalBorderColor = "#101010"
-  , workspaces = myWS
-  , layoutHook = avoidStruts (spacing 5 $ gaps [(U,5),(R,5),(L,5),(D,5)] $ myLayouts)
-  , manageHook = myManage
-  , keys = \c -> myKeys c `M.union` keys gnomeConfig c
-  }
+myManage = composeOne
+  [ isDialog -?> doCenterFloat
+  , role =? "pop-up" -?> doCenterFloat
+  , role =? "bubble" -?> doCenterFloat
+  , role =? "task_dialog" -?> doCenterFloat
+  , role =? "Preferences" -?> doCenterFloat
+  , role =? "page-info" -?> doCenterFloat
+  , className =? "Ninix_main.rb" -?> doIgnore ]
 
 main :: IO ()
 main = do
   spawn "nitrogen --restore"
   spawn "compton"
-  xmonad . withNavigation2DConfig def $
-    myConfig { modMask  = mod1Mask }
+  h <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
+  xmonad . withNavigation2DConfig def $ ewmh def
+    { modMask  = mod1Mask
+    , terminal = "lilyterm"
+    , focusFollowsMouse = False
+    , normalBorderColor = "#101010"
+    , workspaces = myWS
+    , layoutHook = avoidStruts $ (gaps [(U,25),(R,5),(L,5),(D,5)] $ spacing 5 $ myLayouts)
+    , manageHook = myManage <+> manageDocks
+    , keys = myKeys <+> keys def
+    , startupHook = ewmhDesktopsStartup
+    , logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn h }
+    }
