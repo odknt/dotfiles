3246126c5bd3a9ecdc2c5432f9317e4158f2dd35
odknt <4185305+odknt@users.noreply.github.com>
2017-02-23 13:36:33 +0900

    [xmonad] update config

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,4 +1,5 @@
 -- vim: sw=2 ts=2 sts=2 et
+
 import System.Exit
 import XMonad
 import XMonad.Operations
@@ -24,8 +25,7 @@ import qualified Data.Map        as M
 import qualified XMonad.StackSet as W
 
 myLayouts = emptyBSP ||| Tall 1 (3/100) (1/2) ||| Full
-myWS = withScreens 2 ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
--- myWS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+myWS = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 myKeys :: XConfig t -> M.Map (KeyMask, KeySym) (X ())
 myKeys conf@(XConfig { modMask = mask }) = M.fromList $
@@ -33,6 +33,7 @@ myKeys conf@(XConfig { modMask = mask }) = M.fromList $
   , (( mask                 , xK_comma     ), sendMessage (IncMasterN ( 1)))
   , (( mask                 , xK_period    ), sendMessage (IncMasterN (-1)))
   , (( mask                 , xK_b         ), sendMessage $ ToggleStruts)
+  , (( mod4Mask             , xK_l         ), spawn "i3lock -B 3 && xset dpms force off")
   , (( mask                 , xK_d         ), spawn "dmenu_run -fn 'monospace:size=10' -h 25 -w 300 -y 25 -o 0.9 -l 10") ]
   ++ -- Navigation2D
   [ (( mask                 , xK_h         ), windowGo L True)
@@ -71,6 +72,7 @@ myManage = composeOne
 
 main :: IO ()
 main = do
+  nScreens <- countScreens
   spawn "nitrogen --restore"
   spawn "compton"
   h <- spawnPipe "xmobar ~/.xmonad/xmobar.hs"
@@ -79,9 +81,9 @@ main = do
     , terminal = "lilyterm"
     , focusFollowsMouse = False
     , normalBorderColor = "#101010"
-    , workspaces = myWS
-    , layoutHook = avoidStruts $ (gaps [(U,25),(R,5),(L,5),(D,5)] $ spacing 5 $ myLayouts)
-    , manageHook = myManage <+> manageDocks
+    , workspaces = withScreens nScreens myWS
+    , layoutHook = gaps [(U,30),(R,5),(L,5),(D,5)] $ spacing 5 $ myLayouts
+    , manageHook = myManage
     , keys = myKeys <+> keys def
     , startupHook = ewmhDesktopsStartup
     , logHook = dynamicLogWithPP $ def { ppOutput = hPutStrLn h }
