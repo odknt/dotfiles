e4c2077a5765614065ad12a610c9cd7b33c37ec5
odknt <4185305+odknt@users.noreply.github.com>
2017-03-13 15:50:57 +0900

    [general] good bye Ratpoison

--- .ratpoison/scripts/dzen2info.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-fg="#fcfcfc"
-bg="#2d2d2d"
-conky -c ~/.xmonad/scripts/conky_dzen2 | dzen2 -p -ta r -e 'button3=' -fn 'Droid Sans Fallback-8' -fg "$fg" -bg "$bg" -h 24 -w 966 -x 400
--- .ratpoison/scripts/func_mpc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [[ -n "$(mpc current)" ]]; then
-	np=$(mpc current)
-else
-	np="Music off"
-fi
-echo "$np"
