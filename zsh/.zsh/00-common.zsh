9468ac2121e5bda65a435ef7633679bc4d1599f6
odknt <4185305+odknt@users.noreply.github.com>
2018-08-07 17:25:18 +0900

    [general] remove unuded files

--- zsh/.zsh/00-common.zsh
+++
@@ -1,8 +0,0 @@
-zmodload zsh/zpty
-
-ZSH_PLUGIN_DIR=/usr/share/zsh/plugins
-zplug_load() {
-  test -n "${1:-}" \
-  && test -f "${ZSH_PLUGIN_DIR}/${1}/${1}.plugin.zsh" \
-  && . "${ZSH_PLUGIN_DIR}/${1}/"*.plugin.zsh
-}
